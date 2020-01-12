8291ef50c883df995d18a725864463862dd69951
kosadchij <kosadchij@inbox.ru>
2020-01-12 18:42:58 +0300

    Added deleting and updating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])    
   end
 
+  def edit
+    @post = Post.find(params[:id])    
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -18,6 +22,21 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @post = Post.find(params[:id]) 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private def post_params
